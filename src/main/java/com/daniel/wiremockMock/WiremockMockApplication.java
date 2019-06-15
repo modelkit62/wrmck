4cd56af410ccc2c483b359a42e45d1e62e4a5f7c
Daniel <modelkit62@gmail.com>
2019-06-15 22:40:48 +0100

    first commit

---
+++ src/main/java/com/daniel/wiremockMock/WiremockMockApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.wiremockMock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiremockMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiremockMockApplication.class, args);
+	}
+
+}
